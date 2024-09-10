2219d989740c8c53387907468800783022570dee
Ilya Lukyanov <ilya@luk.moe>
2024-09-10 13:06:42 +0100

    add -serve-path

--- server.go
+++ server.go
@@ -7,12 +7,16 @@ import (
 	"io/fs"
 	"net/http"
 	"os"
+	"strings"
 )
 
 //go:embed ui
 var ui embed.FS
 
-var devUIServe = flag.Bool("dev-ui", false, "serve the UI from filesystem instead of embedded")
+var (
+	devUIServe = flag.Bool("dev-ui", false, "serve the UI from filesystem instead of embedded")
+	servePath  = flag.String("serve-path", "/", "path to serve the UI on")
+)
 
 func runServe(addr string) error {
 	if *outputDir == "" {
@@ -23,10 +27,14 @@ func runServe(addr string) error {
 	if *devUIServe {
 		uiFS = os.DirFS("ui")
 	}
-	http.Handle("/", http.FileServer(http.FS(uiFS)))
 
-	http.Handle("/plans/", http.StripPrefix("/plans/", http.FileServer(http.FS(os.DirFS(*outputDir)))))
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.FS(uiFS)))
+	mux.Handle("/plans/", http.StripPrefix("/plans/", http.FileServer(http.FS(os.DirFS(*outputDir)))))
+
+	// otherwise StripPrefix will redirect /foo to foo, which will cause redirect loops
+	*servePath = strings.TrimRight(*servePath, "/")
 
 	fmt.Println("Serving UI on", addr)
-	return http.ListenAndServe(addr, nil)
+	return http.ListenAndServe(addr, http.StripPrefix(*servePath, mux))
 }
